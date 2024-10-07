1d6f0c2903b10a749d7007832299e74c73f97b48
juhwanTV <0412juhwan@gmail.com>
2024-10-08 00:32:51 +0900

    실습 7번

--- 실습/실습_7/소스.cpp
+++ 실습/실습_7/소스.cpp
@@ -1,7 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctime>
 #include <iostream>
+#include <vector>
 #include <gl/glew.h>
 #include <gl/freeglut.h>
 #include <gl/freeglut_ext.h>
@@ -21,60 +23,176 @@ char* filetobuf(const char* file) {
 	fread(buf, length, 1, fptr);
 	fclose(fptr);
 	buf[length] = 0;
-
 	return buf;
 }
+
 using namespace std;
+
+struct ShapeData {
+	vector<GLfloat> positions;
+	vector<GLfloat> colors;
+	GLuint vao, vbo[2];
+};
+
+ShapeData pointData, lineData, triangleData, rectangleData;
+bool drawPointMode = false, drawLineMode = false, drawTriangleMode = false, drawRectangleMode = false;
+bool isLineFirstClick = false;
+GLfloat lineStartX, lineStartY;
 
 void make_vertexShaders();
 void make_fragmentShaders();
 GLuint make_shaderProgram();
 GLvoid drawScene();
 GLvoid Reshape(int w, int h);
-GLvoid Timer(int value);
+GLvoid Keyboard(unsigned char key, int x, int y);
+GLvoid Mouse(int button, int state, int x, int y);
+void updateShapeData(ShapeData& shapeData);
+void updateBuffers();
 
 GLint width, height;
 GLuint shaderProgramID;
 GLuint vertexShader;
 GLuint fragmentShader;
 
-const GLfloat triShape[3][3] = {
-	{-0.5,-0.5,0.0}, {0.5,-0.5,0.0}, {0.0,0.5,0.0} };
+int window_Width = 800;
+int window_Height = 600;
+int selectedShapeIndex = -1;
+int selectedShapeType = -1;
 
-const GLfloat colors[3][3] = {
-   {  1.0,  0.0,  0.0  }, {  0.0,  1.0,  0.0  }, {  0.0,  0.0,  1.0  } };
+const int maxShapeCount = 10;
 
-GLuint vao, vbo[2];
+int getTotalShapeCount() {
+	return (pointData.positions.size() / 3) +
+		(lineData.positions.size() / 6) +
+		(triangleData.positions.size() / 9) +
+		(rectangleData.positions.size() / 12);
+}
 
-void InitBuffer() {
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glGenBuffers(2, vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), triShape, GL_STATIC_DRAW);
+void initializeRandomSeed() {
+	srand(static_cast<unsigned int>(time(0)));
+}
+
+void selectRandomShape() {
+	int totalShapes = getTotalShapeCount();
+	if (totalShapes == 0) {
+		selectedShapeIndex = -1;
+		selectedShapeType = -1;
+		return;
+	}
+
+	int randomShape = rand() % totalShapes;
+	int count = 0;
+
+	if (pointData.positions.size() / 3 > randomShape) {
+		selectedShapeType = 0;
+		selectedShapeIndex = randomShape;
+		return;
+	}
+	count += pointData.positions.size() / 3;
+
+	if (lineData.positions.size() / 6 > randomShape - count) {
+		selectedShapeType = 1;
+		selectedShapeIndex = randomShape - count;
+		return;
+	}
+	count += lineData.positions.size() / 6;
+
+	if (triangleData.positions.size() / 9 > randomShape - count) {
+		selectedShapeType = 2;
+		selectedShapeIndex = randomShape - count;
+		return;
+	}
+	count += triangleData.positions.size() / 9;
+
+	if (rectangleData.positions.size() / 12 > randomShape - count) {
+		selectedShapeType = 3;
+		selectedShapeIndex = randomShape - count;
+		return;
+	}
+	count += rectangleData.positions.size() / 12;
+}
+
+void moveSelectedShape(float dx, float dy) {
+	if (selectedShapeIndex == -1 || selectedShapeType == -1) return;
+
+	vector<GLfloat>* positions = nullptr;
+	int vertexCount = 0;
+
+	switch (selectedShapeType) {
+	case 0:
+		positions = &pointData.positions;
+		vertexCount = 1;
+		break;
+	case 1:
+		positions = &lineData.positions;
+		vertexCount = 2;
+		break;
+	case 2:
+		positions = &triangleData.positions;
+		vertexCount = 3;
+		break;
+	case 3:
+		positions = &rectangleData.positions;
+		vertexCount = 6;
+		break;
+	}
+
+	if (positions) {
+		int baseIndex = selectedShapeIndex * vertexCount * 3;
+		if (baseIndex + (vertexCount * 3) <= positions->size()) {
+			for (int i = 0; i < vertexCount; ++i) {
+				(*positions)[baseIndex + i * 3] += dx;
+				(*positions)[baseIndex + i * 3 + 1] += dy;
+			}
+		}
+	}
+
+	updateBuffers();
+	glutPostRedisplay();
+}
+
+void InitShapeData(ShapeData& shapeData) {
+	glGenVertexArrays(1, &shapeData.vao);
+	glBindVertexArray(shapeData.vao);
+	glGenBuffers(2, shapeData.vbo);
+
+	glBindBuffer(GL_ARRAY_BUFFER, shapeData.vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, 0, nullptr, GL_DYNAMIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-	glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(GLfloat), colors, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, shapeData.vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, 0, nullptr, GL_DYNAMIC_DRAW);
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(1);
+}
+
+void InitBuffer() {
+	InitShapeData(pointData);
+	InitShapeData(lineData);
+	InitShapeData(triangleData);
+	InitShapeData(rectangleData);
 }
 
 void main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(800, 600);
+	glutInitWindowSize(window_Width, window_Height);
 	glutCreateWindow("Example7");
 
 	glewExperimental = GL_TRUE;
 	glewInit();
+
+	initializeRandomSeed();
 
 	make_shaderProgram();
 	InitBuffer();
 
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(Reshape);
+	glutKeyboardFunc(Keyboard);
+	glutMouseFunc(Mouse);
 
 	glutMainLoop();
 }
@@ -108,7 +226,6 @@ void make_fragmentShaders() {
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
 	glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
-
 	glCompileShader(fragmentShader);
 
 	GLint result;
@@ -135,28 +252,204 @@ GLuint make_shaderProgram() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
 	glUseProgram(shaderProgramID);
 	return shaderProgramID;
 }
 
 GLvoid drawScene() {
-	GLuint shaderID = glCreateProgram();
-
 	glClearColor(1.0, 1.0, 1.0, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glUseProgram(shaderProgramID);
 
-	glBindVertexArray(vao);
+	auto drawShape = [](ShapeData& shapeData, GLenum mode, float pointSize = 1.0f) {
+		if (!shapeData.positions.empty()) {
+			glBindVertexArray(shapeData.vao);
 
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+			if (mode == GL_POINTS) {
+				glPointSize(pointSize);
+			}
+			glDrawArrays(mode, 0, shapeData.positions.size() / 3);
+		}
+	};
+
+	drawShape(pointData, GL_POINTS, 5.0f);
+	drawShape(lineData, GL_LINES);
+	drawShape(triangleData, GL_TRIANGLES);
+	drawShape(rectangleData, GL_TRIANGLES);
 
 	glutSwapBuffers();
 }
 
 GLvoid Reshape(int w, int h) {
 	glViewport(0, 0, w, h);
+}
+
+GLvoid Mouse(int button, int state, int x, int y) {
+	GLfloat x_opengl = (2.0f * x / window_Width) - 1.0f;
+	GLfloat y_opengl = 1.0f - (2.0f * y / window_Height);
+
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		if (getTotalShapeCount() > maxShapeCount) {
+			return;
+		}
+
+		if (drawPointMode) {			
+			drawPointMode = false;
+
+			pointData.positions.push_back(x_opengl);
+			pointData.positions.push_back(y_opengl);
+			pointData.positions.push_back(0.0f);
+
+			pointData.colors.push_back(0.0f);
+			pointData.colors.push_back(0.0f);
+			pointData.colors.push_back(1.0f);
+
+			updateShapeData(pointData);
+			glutPostRedisplay();
+		}
+		else if(drawLineMode) {
+			if (!isLineFirstClick) {
+				lineStartX = x_opengl;
+				lineStartY = y_opengl;
+				isLineFirstClick = true;
+			}
+			else {
+				isLineFirstClick = false;
+				drawLineMode = false;
+
+				lineData.positions.push_back(lineStartX);
+				lineData.positions.push_back(lineStartY);
+				lineData.positions.push_back(0.0f);
+
+				lineData.positions.push_back(x_opengl);
+				lineData.positions.push_back(y_opengl);
+				lineData.positions.push_back(0.0f);
+
+				lineData.colors.push_back(0.0f);
+				lineData.colors.push_back(1.0f);
+				lineData.colors.push_back(0.0f);
+
+				lineData.colors.push_back(0.0f);
+				lineData.colors.push_back(1.0f);
+				lineData.colors.push_back(0.0f);
+
+				updateShapeData(lineData);
+				glutPostRedisplay();
+			}
+		}
+		else if (drawTriangleMode) {
+			drawTriangleMode = false;
+
+			GLfloat size = 0.1f;
+			GLfloat x1 = x_opengl;
+			GLfloat y1 = y_opengl + size;
+
+			GLfloat x2 = x_opengl - size * 0.886f;
+			GLfloat y2 = y_opengl - size * 0.5f;
+
+			GLfloat x3 = x_opengl + size * 0.886f;
+			GLfloat y3 = y_opengl - size * 0.5f;
+
+			triangleData.positions.insert(triangleData.positions.end(), { x1, y1, 0.0f, x2, y2, 0.0f, x3, y3, 0.0f });
+			triangleData.colors.insert(triangleData.colors.end(), { 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f });
+
+			updateShapeData(triangleData);
+			glutPostRedisplay();
+		}
+		else if (drawRectangleMode) {
+			drawRectangleMode = false;
+
+			GLfloat width = 0.2f, height = 0.1f;
+
+			GLfloat x1 = x_opengl;
+			GLfloat y1 = y_opengl;
+			GLfloat x2 = x_opengl + width;
+			GLfloat y2 = y_opengl;
+			GLfloat x3 = x_opengl;
+			GLfloat y3 = y_opengl - height;
+			GLfloat x4 = x_opengl + width;
+			GLfloat y4 = y_opengl - height;
+
+			rectangleData.positions.insert(rectangleData.positions.end(), {
+				x1, y1, 0.0f, x2, y2, 0.0f, x3, y3, 0.0f,
+				x2, y2, 0.0f, x4, y4, 0.0f, x3, y3, 0.0f
+				});
+
+			rectangleData.colors.insert(rectangleData.colors.end(), {
+				1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f,
+				1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f
+				});
+
+			updateShapeData(rectangleData);
+			glutPostRedisplay();
+		}
+	}
+}
+
+GLvoid Keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+	case 'p':
+		drawPointMode = true;
+		break;
+	case 'l':
+		drawLineMode = true;
+		isLineFirstClick = false;
+		break;
+	case 't':
+		drawTriangleMode = true;
+		break;
+	case 'r':
+		drawRectangleMode = true;
+		break;
+	case 'w':
+		selectRandomShape();
+		moveSelectedShape(0.0f, 0.05f);
+		break;
+	case 'a':
+		selectRandomShape();
+		moveSelectedShape(-0.05f, 0.0f);
+		break;
+	case 's':
+		selectRandomShape();
+		moveSelectedShape(0.0f, -0.05f);
+		break;
+	case 'd':
+		selectRandomShape();
+		moveSelectedShape(0.05f, 0.0f);
+		break;
+	case 'c':
+		pointData.positions.clear();
+		pointData.colors.clear();
+		lineData.positions.clear();
+		lineData.colors.clear();
+		triangleData.positions.clear();
+		triangleData.colors.clear();
+		rectangleData.positions.clear();
+		rectangleData.colors.clear();
+
+		updateBuffers();
+		glutPostRedisplay();
+		break;
+	case 'q':
+		glutLeaveMainLoop();
+		break;
+	}
+}
+
+void updateShapeData(ShapeData& shapeData) {
+	glBindVertexArray(shapeData.vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, shapeData.vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, shapeData.positions.size() * sizeof(GLfloat), shapeData.positions.data(), GL_DYNAMIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, shapeData.vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, shapeData.colors.size() * sizeof(GLfloat), shapeData.colors.data(), GL_DYNAMIC_DRAW);
+}
+
+void updateBuffers() {
+	updateShapeData(pointData);
+	updateShapeData(lineData);
+	updateShapeData(triangleData);
+	updateShapeData(rectangleData);
 }
